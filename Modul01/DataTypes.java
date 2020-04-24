f3b911e636675a89b747a54989849ef965caa131
rahmatulaina <rahmatulaina28@gmail.com>
2020-04-24 13:10:05 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main (String [] args){
+
+  String namaDepan = "Rahmatul";
+  String namaBelakang = "Aina";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean cantik = true;
+
+ }
+}
